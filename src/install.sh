c5136c2ffe4c717126527d5d060fe2c276bb23b6
Rodrigo Thiago Brun <rodrigotbrun@gmail.com>
2017-10-18 13:37:58 -0200

    updated install script

--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d "/home/nw-backup" ]
 then
-	echo -e "\e[1m Creating working user... (please provide sudo access)\e[0m"
+	echo -e "\033[1m Creating working user... (please provide sudo access)\e[0m"
 	sudo adduser nw-backup
 	sudo mkdir /home/nw-backup
 	sudo chown nw-backup:nw-backup /home/nw-backup
@@ -16,5 +16,31 @@ then
   mkdir groups && chmod 777 groups
 fi
 
-wget https://raw.githubusercontent.com/newestapps/backup-agent/master/src/nw-backup.sh
+## Check dependencies
+
+hasZipInstalled=`command -v zip`
+hasUnzipInstalled=`command -v unzip`
+hasAwsCliInstalled=`command -v aws`
+
+if [ ! -n "$hasZipInstalled" ]
+then
+  echo -e "\033[31;1m /!\ ZIP is not installed /!\ \033[0m"
+  exit
+fi
+
+if [ ! -n "$hasUnzipInstalled" ]
+then
+  echo -e "\033[31;1m /!\ UNZIP is not installed /!\ \033[0m"
+  exit
+fi
+
+if [ ! -n "$hasAwsCliInstalled" ]
+then
+  echo -e "\033[31;1m /!\ AWS CLI is not installed /!\ \033[0m"
+  exit
+fi
+
+wget https://raw.githubusercontent.com/newestapps/backup-agent/master/src/nw-backup.sh -q
 chmod 777 nw-backup.sh
+
+echo -e "\033[1m Ready to go! \033[0m"
